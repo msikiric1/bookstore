5fa4bca4b4a0054d949a10d74ba13c88f709c3ca
msikiric1 <msikiric1@etf.unsa.ba>
2022-12-25 03:02:03 +0100

    add book category model class

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Category.java
@@ -0,0 +1,49 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Objects;
+
+/**
+ * Bean class for representing book categories
+ * @author Muaz Sikiric
+ */
+public class Category {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}

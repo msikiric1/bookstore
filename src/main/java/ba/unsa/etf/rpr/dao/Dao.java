c8144af5bd3a72e56ef5529bd2507ca13b6df1de
msikiric1 <msikiric1@etf.unsa.ba>
2022-12-25 03:19:42 +0100

    finish dao interface

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
+import java.util.List;
+
 /**
  * Root interface for all DAO classes
  * @author Muaz Sikiric
@@ -11,4 +13,29 @@ public interface Dao<T> {
      * @return entity from database
      */
     T getById(int id);
+
+    /**
+     * Adds an entity to database
+     * @param item item to add
+     */
+    void save(T item);
+
+    /**
+     * Updates an entity in database based on it's ID
+     * @param id primary key of an entity
+     * @return updated entity from database
+     */
+    T update(int id);
+
+    /**
+     * Deletes and entity from database base on it's ID
+     * @param id primary key of an entity
+     */
+    void delete(int id);
+
+    /**
+     * Returns all entities from database
+     * @return List of all entities from database
+     */
+    List<T> getAll();
 }

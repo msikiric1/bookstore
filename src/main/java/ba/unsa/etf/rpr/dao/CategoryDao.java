d9d4657137653d6837b0c04b4014247f1eee9e54
msikiric1 <msikiric1@etf.unsa.ba>
2022-12-25 03:21:58 +0100

    add CategoryDao class

---
+++ src/main/java/ba/unsa/etf/rpr/dao/CategoryDao.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Category;
+
+/**
+ * Dao interface for Category model
+ * @author Muaz Sikiric
+ */
+public interface CategoryDao extends Dao<Category> {
+}

27e50e8e31851d451caf7972eee921eb87c2e008
msikiric1 <msikiric1@etf.unsa.ba>
2022-12-25 16:18:26 +0100

    add throws keyword with custom exception to all methods in Dao class

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.exceptions.BookstoreException;
+
 import java.util.List;
 
 /**
@@ -12,30 +14,30 @@ public interface Dao<T> {
      * @param id primary key of an entity
      * @return entity from database
      */
-    T getById(int id);
+    T getById(int id) throws BookstoreException;
 
     /**
      * Adds an entity to database
      * @param item item to add
      */
-    void save(T item);
+    void save(T item) throws BookstoreException;
 
     /**
      * Updates an entity in database based on it's ID
-     * @param id primary key of an entity
+     * @param item primary key of an entity
      * @return updated entity from database
      */
-    T update(T item);
+    T update(T item) throws BookstoreException;
 
     /**
      * Deletes and entity from database base on it's ID
      * @param id primary key of an entity
      */
-    void delete(int id);
+    void delete(int id) throws BookstoreException;
 
     /**
      * Returns all entities from database
      * @return List of all entities from database
      */
-    List<T> getAll();
+    List<T> getAll() throws BookstoreException;
 }

252ef174098518374ed6a1cf291d179ddefad14b
msikiric1 <msikiric1@etf.unsa.ba>
2022-12-25 16:27:05 +0100

    add identifiable interface that forces classes that implement it to have setters and getters for id attribute

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Identifiable.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.domain;
+
+/**
+ * Interface tha forces all POJO classes that implement it to have setters and getters
+ * for id (forcing them to also have id attribute)
+ * @author Muaz Sikiric
+ */
+public interface Identifiable {
+    void getId();
+    
+    void setId(int id);
+}

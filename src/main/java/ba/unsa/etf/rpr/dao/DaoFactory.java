bdc616cd5615349b5f9dd89ea2768e796be1b661
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-15 22:03:09 +0100

    start dao factory implementation

---
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.dao;
+
+/**
+ * Factory class used for singleton implementations of DAO classes
+ * @author Muaz Sikiric
+ */
+public class DaoFactory {
+
+    private DaoFactory() {}
+
+
+}

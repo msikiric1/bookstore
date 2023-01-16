f2b5958aa686f9abbb42ced05c529a08254e43e0
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-16 03:17:33 +0100

    add UserDao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.User;
+
+/**
+ * Dao interface for User model
+ * @author Muaz Sikiric
+ */
+public interface UserDao extends Dao<User> {
+}

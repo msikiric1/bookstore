a43822a4a777e90b3457f4dae3d44804ff3ad895
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-16 13:32:39 +0100

    start implementation registration form

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -1,4 +1,26 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class RegistrationController {
+    public TextField usernameField;
+    public PasswordField passwordField;
+    public PasswordField confirmPasswordField;
+    public Label errorMsgLabel;
+    public Button goBackBtn;
+    public Button registerBtn;
+
+    public void initialize() {
+
+    }
+
+    public void registerClick(ActionEvent actionEvent) {
+    }
+
+    public void goBackClick(ActionEvent actionEvent) {
+    }
 }

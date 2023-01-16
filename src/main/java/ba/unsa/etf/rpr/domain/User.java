e5d66670ea4592fd362a5b606ab807e29f89cfb9
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-16 03:13:07 +0100

    add User class

---
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -0,0 +1,69 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Objects;
+
+/**
+ * Bean class for representing users and admins
+ * @author Muaz Sikiric
+ */
+public class User implements Identifiable {
+    private int id;
+    private String username;
+    private String password;
+    private boolean isAdmin;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", isAdmin=" + isAdmin +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && isAdmin == user.isAdmin && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, isAdmin);
+    }
+}

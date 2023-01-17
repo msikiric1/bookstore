b9766f775b1f49d93bd0198d4df86d09e20c1397
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-18 00:33:38 +0100

    update RegistrationController to simplify changing windows

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -8,6 +8,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -75,36 +76,37 @@ public class RegistrationController {
             return;
         }
 
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-            stage.getIcons().add(new Image("/images/bookstore_icon.png"));
-            stage.setTitle("Bookstore | Main");
-            stage.setResizable(false);
-            stage.show();
-
-            Stage currentStage = (Stage) registerBtn.getScene().getWindow();
-            currentStage.close();
-        } catch (IOException e) {
-            throw new BookstoreException(e.getMessage(), e);
-        }
+        changeWindow("main", "Main", new MainController(usernameField.getText()), actionEvent);
     }
 
     public void goBackClick(ActionEvent actionEvent) throws BookstoreException {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/fxml/home.fxml"));
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-            stage.getIcons().add(new Image("/images/bookstore_icon.png"));
-            stage.setTitle("Bookstore | Home");
-            stage.setResizable(false);
-            stage.show();
+        changeWindow("home", "Home", new HomeController(), actionEvent);
+    }
 
-            Stage currentStage = (Stage) registerBtn.getScene().getWindow();
-            currentStage.close();
+    /**
+     * Helper function used for displaying a different window
+     * @param fxmlFileName name of the FXML file
+     * @param title title that will be displayed as "Bookstore | 'title'"
+     * @param controller controller for the new window
+     * @param actionEvent actionEvent parameter that is passed down by the method that called this method
+     * @throws BookstoreException exception is thrown if a file with given name does not exist
+     */
+    private void changeWindow(String fxmlFileName, String title, Object controller, ActionEvent actionEvent) throws BookstoreException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/" + fxmlFileName + ".fxml"));
+        loader.setController(controller);
+        Stage newStage = new Stage();
+        try {
+            newStage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         } catch (IOException e) {
-            throw new BookstoreException(e.getMessage(), e);
+            throw new BookstoreException("FXML file does not exist.");
         }
+        newStage.getIcons().add(new Image("/images/bookstore_icon.png"));
+        newStage.setTitle("Bookstore | " + title);
+        newStage.setResizable(false);
+        newStage.show();
+
+        Node n = (Node) actionEvent.getSource();
+        Stage stage = (Stage) n.getScene().getWindow();
+        stage.close();
     }
 }

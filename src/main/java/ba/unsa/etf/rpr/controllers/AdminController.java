6293005d40e6f8a4f0d714dde7b2eae96f38a4b7
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-17 22:42:18 +0100

    add fxid for elements in admin panel

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
@@ -1,4 +1,20 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class AdminController {
+    public TableView booksTable;
+    public TableColumn booksColId;
+    public TableColumn booksColTitle;
+    public TableColumn booksColAuthorId;
+    public TableColumn booksColPublished;
+    public TableColumn booksColPrice;
+    public TableColumn booksColCategoryId;
+    public Button logoutBtn;
+    public Button closeBtn;
+    public Button deleteBookBtn;
+    public Button viewCategoriesBtn;
+    public Button viewAuthorsBtn;
 }

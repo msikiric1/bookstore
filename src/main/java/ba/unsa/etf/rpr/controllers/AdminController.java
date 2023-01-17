0e361919bce3354d9b692075cdd16d89bcade61f
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-17 23:00:21 +0100

    add show username and close button functionality

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminController.java
@@ -1,8 +1,12 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
 public class AdminController {
     public TableView booksTable;
@@ -17,4 +21,33 @@ public class AdminController {
     public Button deleteBookBtn;
     public Button viewCategoriesBtn;
     public Button viewAuthorsBtn;
+    public Label usernameLabel;
+    private String username;
+
+    public AdminController(String username) {
+        this.username = username;
+    }
+
+    @FXML
+    public void initialize() {
+        usernameLabel.setText(usernameLabel.getText() + username);
+    }
+
+
+    public void logoutAction(ActionEvent actionEvent) {
+
+    }
+
+    public void closeAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) usernameLabel.getScene().getWindow();
+        stage.close();
+    }
+
+    public void viewCategoriesAction(ActionEvent actionEvent) {
+    }
+
+    public void viewAuthorsAction(ActionEvent actionEvent) {
+    }
+
+
 }

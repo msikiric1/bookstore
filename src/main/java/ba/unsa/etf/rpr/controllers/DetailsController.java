9033dfce267f35ecc8c05833aac8bfbf205b9f71
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-18 16:09:17 +0100

    add method to generate details based on book or author

--- src/main/java/ba/unsa/etf/rpr/controllers/DetailsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/DetailsController.java
@@ -1,4 +1,45 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.domain.Author;
+import ba.unsa.etf.rpr.domain.Book;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
 public class DetailsController {
+    public Label pageLabel;
+    public VBox detailsBox;
+    private Book book = null;
+    private Author author = null;
+    private String pageTitle;
+
+    public DetailsController(Object object) {
+        if (object instanceof Book) {
+            this.book = (Book) object;
+            this.pageTitle = "Book Details";
+        } else if (object instanceof Author) {
+            author = (Author) object;
+            this.pageTitle = "Author Details";
+        }
+    }
+    @FXML
+    public void initialize() {
+        pageLabel.setText(pageTitle);
+        generateDetails();
+    }
+    public void closeAction(ActionEvent actionEvent) {
+    }
+
+    private void generateDetails() {
+        if (book != null) {
+            detailsBox.getChildren().add(new Label("Title: " + book.getTitle()));
+            detailsBox.getChildren().add(new Label("Author: " + book.getAuthor().getName()));
+            detailsBox.getChildren().add(new Label("Published: " + book.getPublished()));
+            detailsBox.getChildren().add(new Label("Price: " + book.getPrice() + "$"));
+            detailsBox.getChildren().add(new Label("Category: " + book.getCategory().getName()));
+        } else {
+
+        }
+    }
 }

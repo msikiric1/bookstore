707b2bb4bd9baf6d83c6832efe0985d4aee5cd4f
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-18 16:43:10 +0100

    update changeWindow method and start working on details button functionality

--- src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
@@ -30,7 +30,7 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class MainController {
     public Label usernameLabel;
     public TextField searchField;
-    public ListView booksListView;
+    public ListView<Book> booksListView;
     public VBox categoriesBox;
     private ObservableList<Book> books;
     private ObservableList<Book> filteredBooks;
@@ -87,6 +87,10 @@ public class MainController {
     }
 
     public void detailsAction(ActionEvent actionEvent) {
+        Book selectedBook = filteredBooks.stream().filter(book -> {
+            return booksListView.getSelectionModel().getSelectedItem().getId() == book.getId();
+        }).findAny().orElse(null);
+        ;
     }
 
     private void searchBooks(String searchString) {
@@ -99,33 +103,6 @@ public class MainController {
         booksListView.setItems(filteredBooks);
     }
 
-    /**
-     * Helper function used for displaying a different window
-     * @param fxmlFileName name of the FXML file
-     * @param title title that will be displayed as "Bookstore | 'title'"
-     * @param controller controller for the new window
-     * @param actionEvent actionEvent parameter that is passed down by the method that called this method
-     * @throws BookstoreException exception is thrown if a file with given name does not exist
-     */
-    private void changeWindow(String fxmlFileName, String title, Object controller, ActionEvent actionEvent) throws BookstoreException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/" + fxmlFileName + ".fxml"));
-        loader.setController(controller);
-        Stage newStage = new Stage();
-        try {
-            newStage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        } catch (IOException e) {
-            throw new BookstoreException("FXML file does not exist.");
-        }
-        newStage.getIcons().add(new Image("/images/bookstore_icon.png"));
-        newStage.setTitle("Bookstore | " + title);
-        newStage.setResizable(false);
-        newStage.show();
-
-        Node n = (Node) actionEvent.getSource();
-        Stage stage = (Stage) n.getScene().getWindow();
-        stage.close();
-    }
-
     public void addAction(ActionEvent actionEvent) throws BookstoreException {
         AoUController aouController = new AoUController("Add", authors, categories);
         openWindow("aoubook", "Add a book", aouController, actionEvent);
@@ -135,6 +112,22 @@ public class MainController {
     }
 
     public void deleteAction(ActionEvent actionEvent) {
+    }
+
+    /**
+     * Helper function used for displaying a different window
+     * @param fxmlFileName name of the FXML file
+     * @param title title that will be displayed as "Bookstore | 'title'"
+     * @param controller controller for the new window
+     * @param actionEvent actionEvent parameter that is passed down by the method that called this method
+     * @throws BookstoreException exception is thrown if a file with given name does not exist
+     */
+    private void changeWindow(String fxmlFileName, String title, Object controller, ActionEvent actionEvent) throws BookstoreException {
+        openWindow(fxmlFileName, title, controller, actionEvent);
+
+        Node n = (Node) actionEvent.getSource();
+        Stage stage = (Stage) n.getScene().getWindow();
+        stage.close();
     }
 
     private void openWindow(String fxmlFileName, String title, Object controller, ActionEvent actionEvent) throws BookstoreException {

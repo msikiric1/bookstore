ed1ab9b691c2b2f4bf7ef15d2692728fc870ba7b
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-18 06:14:57 +0100

    add more functionality to AoUController and start implementing validation

--- src/main/java/ba/unsa/etf/rpr/controllers/AoUController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AoUController.java
@@ -1,12 +1,16 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.domain.Author;
+import ba.unsa.etf.rpr.domain.Book;
 import ba.unsa.etf.rpr.domain.Category;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
 
 public class AoUController {
     public Label pageLabel;
@@ -15,6 +19,7 @@ public class AoUController {
     public DatePicker publishedPicker;
     public ComboBox authorCbox;
     public ComboBox categoryCbox;
+    public Button submitBtn;
     private String addOrUpdate;
     private ObservableList<Author> authors;
     private ObservableList<Category> categories;
@@ -27,12 +32,34 @@ public class AoUController {
 
     @FXML
     public void initialize() {
-        authorCbox = new ComboBox(FXCollections.observableArrayList(authors));
+        pageLabel.setText(addOrUpdate + titleField.getText());
+        authorCbox.getItems().addAll(authors);
+        authorCbox.getSelectionModel().selectFirst();
+        categoryCbox.getItems().addAll(categories);
+        categoryCbox.getSelectionModel().selectFirst();
+        submitBtn.setText(addOrUpdate);
+        if(addOrUpdate.equals("Add")) {
+            publishedPicker.setValue(LocalDate.now());
+        } else {
+
+        }
     }
 
     public void submitAction(ActionEvent actionEvent) {
+        Book book = new Book();
+        validate(titleField, publishedPicker);
+        book.setTitle();
+        try {
+
+        } catch() {
+
+        }
     }
 
     public void cancelAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) titleField.getScene().getWindow();
+        stage.close();
     }
+
+
 }

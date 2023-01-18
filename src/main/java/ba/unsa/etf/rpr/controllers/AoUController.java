d7a4fe3236d9497ef296b34d19de192d1ac5f8a9
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-18 05:46:46 +0100

    start implementation of add or update book window

--- src/main/java/ba/unsa/etf/rpr/controllers/AoUController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AoUController.java
@@ -1,4 +1,38 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.domain.Author;
+import ba.unsa.etf.rpr.domain.Category;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class AoUController {
+    public Label pageLabel;
+    public Spinner priceSpinner;
+    public TextField titleField;
+    public DatePicker publishedPicker;
+    public ComboBox authorCbox;
+    public ComboBox categoryCbox;
+    private String addOrUpdate;
+    private ObservableList<Author> authors;
+    private ObservableList<Category> categories;
+
+    public AoUController(String addOrUpdate, ObservableList<Author> authors, ObservableList<Category> categories) {
+        this.addOrUpdate = addOrUpdate;
+        this.authors = authors;
+        this.categories = categories;
+    }
+
+    @FXML
+    public void initialize() {
+        authorCbox = new ComboBox(FXCollections.observableArrayList(authors));
+    }
+
+    public void submitAction(ActionEvent actionEvent) {
+    }
+
+    public void cancelAction(ActionEvent actionEvent) {
+    }
 }

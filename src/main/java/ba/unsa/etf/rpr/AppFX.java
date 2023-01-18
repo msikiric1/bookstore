60af0264f12300274250e49fb29ed0e85f6f7dd8
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-18 03:34:45 +0100

    finalize login panel design and completely remove redundant home panel

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,10 +1,9 @@
 package ba.unsa.etf.rpr;
 
 import ba.unsa.etf.rpr.controllers.HomeController;
+import ba.unsa.etf.rpr.controllers.LoginController;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -14,10 +13,9 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class AppFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/home.fxml"));
-        HomeController homeController = new HomeController();
-        loader.setController(homeController);
-        stage.setTitle("Bookstore | Home");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        loader.setController(new LoginController());
+        stage.setTitle("Bookstore | Login");
         stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         stage.getIcons().add(new Image("/images/bookstore_icon.png"));
         stage.setResizable(false);
--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -6,11 +6,19 @@ import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.BookstoreException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 /**
  *
@@ -23,38 +31,84 @@ public class LoginController {
     public Button closeBtn;
     public Label errorMsgLabel;
 
+    public LoginController() {}
+
     @FXML
     public void initialize() {
-        usernameField.getStyleClass().add("invalid");
+        errorMsgLabel.setVisible(false);
         usernameField.textProperty().addListener((observableValue, o, n) -> {
             if(n.trim().isEmpty()) {
-                usernameField.getStyleClass().removeAll("valid");
+                usernameField.getStyleClass().removeAll("default");
                 usernameField.getStyleClass().add("invalid");
             } else {
                 usernameField.getStyleClass().removeAll("invalid");
-                usernameField.getStyleClass().add("valid");
+                usernameField.getStyleClass().add("default");
+            }
+        });
+        passwordField.textProperty().addListener((observableValue, o, n) -> {
+            if(n.trim().isEmpty()) {
+                passwordField.getStyleClass().removeAll("default");
+                passwordField.getStyleClass().add("invalid");
+            } else {
+                passwordField.getStyleClass().removeAll("invalid");
+                passwordField.getStyleClass().add("default");
             }
         });
     }
 
-    public void loginClick(ActionEvent actionEvent) {
-        User user = new User();
+    public void loginAction(ActionEvent actionEvent) throws BookstoreException {
+        User user;
         try {
             user = new UserDaoSQLImpl().getUser(usernameField.getText(), passwordField.getText());
         } catch(BookstoreException e) {
             errorMsgLabel.setText("The username or password is incorrect.");
+            errorMsgLabel.setVisible(true);
             return;
         }
 
         if(user.isAdmin()) {
             System.out.println("admin");
-        } else {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/admin.fxml"));
+                AdminController adminController = new AdminController(usernameField.getText());
+                loader.setController(adminController);
+                Stage stage = new Stage();
+                stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+                stage.getIcons().add(new Image("/images/bookstore_icon.png"));
+                stage.setTitle("Bookstore | Admin");
+                stage.setResizable(false);
+                stage.show();
 
+                Stage currentStage = (Stage) loginBtn.getScene().getWindow();
+                currentStage.close();
+            } catch (IOException e) {
+                throw new BookstoreException(e.getMessage(), e);
+            }
+        } else {
+            System.out.println("user");
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+                stage.getIcons().add(new Image("/images/bookstore_icon.png"));
+                stage.setTitle("Bookstore | Main");
+                stage.setResizable(false);
+                stage.show();
+
+                Stage currentStage = (Stage) loginBtn.getScene().getWindow();
+                currentStage.close();
+            } catch (IOException e) {
+                throw new BookstoreException(e.getMessage(), e);
+            }
         }
     }
 
-    public void closeClick(ActionEvent actionEvent) {
+    public void closeAction(ActionEvent actionEvent) {
         Stage stage = (Stage) loginBtn.getScene().getWindow();
         stage.close();
+    }
+
+    public void goToRegistrationAction(ActionEvent actionEvent) {
+
     }
 }

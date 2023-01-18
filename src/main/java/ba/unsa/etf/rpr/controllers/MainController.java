8599ccbacef844d28dd5573e9e6b8027931c3269
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-18 15:13:22 +0100

    add close and logout button functionality

--- src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
@@ -9,11 +9,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.stream.Collectors;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class MainController {
     public Label usernameLabel;
@@ -48,10 +56,13 @@ public class MainController {
         });
     }
 
-    public void logoutAction(ActionEvent actionEvent) {
+    public void logoutAction(ActionEvent actionEvent) throws BookstoreException {
+        changeWindow("login", "Login", new LoginController(), actionEvent);
     }
 
     public void closeAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) usernameLabel.getScene().getWindow();
+        stage.close();
     }
 
     public void searchAction(ActionEvent actionEvent) {
@@ -68,5 +79,58 @@ public class MainController {
 
     private void refresh() {
         booksListView.setItems(filteredBooks);
+    }
+
+    /**
+     * Helper function used for displaying a different window
+     * @param fxmlFileName name of the FXML file
+     * @param title title that will be displayed as "Bookstore | 'title'"
+     * @param controller controller for the new window
+     * @param actionEvent actionEvent parameter that is passed down by the method that called this method
+     * @throws BookstoreException exception is thrown if a file with given name does not exist
+     */
+    private void changeWindow(String fxmlFileName, String title, Object controller, ActionEvent actionEvent) throws BookstoreException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/" + fxmlFileName + ".fxml"));
+        loader.setController(controller);
+        Stage newStage = new Stage();
+        try {
+            newStage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        } catch (IOException e) {
+            throw new BookstoreException("FXML file does not exist.");
+        }
+        newStage.getIcons().add(new Image("/images/bookstore_icon.png"));
+        newStage.setTitle("Bookstore | " + title);
+        newStage.setResizable(false);
+        newStage.show();
+
+        Node n = (Node) actionEvent.getSource();
+        Stage stage = (Stage) n.getScene().getWindow();
+        stage.close();
+    }
+
+    public void addAction(ActionEvent actionEvent) throws BookstoreException {
+        AoUController aouController = new AoUController("Add", authors, categories);
+        openWindow("aoubook", "Add a book", aouController, actionEvent);
+    }
+
+    public void updateAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteAction(ActionEvent actionEvent) {
+    }
+
+    private void openWindow(String fxmlFileName, String title, Object controller, ActionEvent actionEvent) throws BookstoreException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/" + fxmlFileName + ".fxml"));
+        loader.setController(controller);
+        Stage newStage = new Stage();
+        try {
+            newStage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        } catch (IOException e) {
+            throw new BookstoreException("FXML file does not exist.");
+        }
+        newStage.getIcons().add(new Image("/images/bookstore_icon.png"));
+        newStage.setTitle("Bookstore | " + title);
+        newStage.setResizable(false);
+        newStage.show();
     }
 }

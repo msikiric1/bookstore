58a2a80e9c4cb17cf1593c67649bc2374bd03d41
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-21 23:29:40 +0100

    add methods for opening and closing a window

--- src/main/java/ba/unsa/etf/rpr/business/WindowManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/WindowManager.java
@@ -12,9 +12,13 @@ import java.io.IOException;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
+/**
+ *
+ * @author Muaz Sikiric
+ */
 public class WindowManager {
     /**
-     * Used for displaying a different window instead of the current one
+     * Displays a different window instead of the current one
      * @param fxmlFileName name of the FXML file
      * @param title title that will be displayed as "Bookstore | 'title'"
      * @param controller controller for the new window
@@ -22,23 +26,41 @@ public class WindowManager {
      * @throws BookstoreException exception is thrown if a file with given name does not exist
      */
     private void changeWindow(String fxmlFileName, String title, Object controller, ActionEvent actionEvent) throws BookstoreException {
+        openWindow(fxmlFileName, title, controller, actionEvent);
+        closeWindow(actionEvent);
+    }
+
+    /**
+     * Opens a new window
+     * @param fxmlFileName name of the FXML file
+     * @param title title that will be displayed as "Bookstore | 'title'"
+     * @param controller controller for the new window
+     * @param actionEvent an event representing some type of action
+     * @throws BookstoreException exception is thrown if a file with given name does not exist
+     */
+    private Stage openWindow(String fxmlFileName, String title, Object controller, ActionEvent actionEvent) throws BookstoreException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/" + fxmlFileName + ".fxml"));
         loader.setController(controller);
         Stage newStage = new Stage();
         try {
             newStage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         } catch (IOException e) {
-            throw new BookstoreException("FXML file does not exist.");
+            throw new BookstoreException(e.getMessage(), e);
         }
         newStage.getIcons().add(new Image("/images/bookstore_icon.png"));
         newStage.setTitle("Bookstore | " + title);
         newStage.setResizable(false);
         newStage.show();
+        return newStage;
+    }
 
+    /**
+     * Closes the current window
+     * @param actionEvent an event representing some type of action
+     */
+    public void closeWindow(ActionEvent actionEvent) {
         Node n = (Node) actionEvent.getSource();
         Stage stage = (Stage) n.getScene().getWindow();
         stage.close();
     }
-
-
 }

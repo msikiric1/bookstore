7be462430d90e3a7ce364d00c4b19b9107b352e5
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-21 23:22:18 +0100

    add WindowManager class that will contain methods for changing windows, opening new ones, closing them etc.

---
+++ src/main/java/ba/unsa/etf/rpr/business/WindowManager.java
@@ -0,0 +1,44 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.exceptions.BookstoreException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class WindowManager {
+    /**
+     * Used for displaying a different window instead of the current one
+     * @param fxmlFileName name of the FXML file
+     * @param title title that will be displayed as "Bookstore | 'title'"
+     * @param controller controller for the new window
+     * @param actionEvent an event representing some type of action
+     * @throws BookstoreException exception is thrown if a file with given name does not exist
+     */
+    private void changeWindow(String fxmlFileName, String title, Object controller, ActionEvent actionEvent) throws BookstoreException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/" + fxmlFileName + ".fxml"));
+        loader.setController(controller);
+        Stage newStage = new Stage();
+        try {
+            newStage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        } catch (IOException e) {
+            throw new BookstoreException("FXML file does not exist.");
+        }
+        newStage.getIcons().add(new Image("/images/bookstore_icon.png"));
+        newStage.setTitle("Bookstore | " + title);
+        newStage.setResizable(false);
+        newStage.show();
+
+        Node n = (Node) actionEvent.getSource();
+        Stage stage = (Stage) n.getScene().getWindow();
+        stage.close();
+    }
+
+
+}

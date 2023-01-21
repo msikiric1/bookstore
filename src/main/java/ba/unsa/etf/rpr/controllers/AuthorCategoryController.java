5dbcd1ea551c9559076bde0055225dded64330fc
msikiric1 <msikiric1@etf.unsa.ba>
2023-01-21 15:17:57 +0100

    add controller for authors and categories panel, and remove home controller

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/AuthorCategoryController.java
@@ -0,0 +1,38 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class AuthorCategoryController {
+    public TextField nameField;
+    public TextField addressField;
+    public TextField phoneField;
+    public ComboBox typeCbox;
+    public TableView authorsTable;
+    public TableColumn authorsColId;
+    public TableColumn authorsColName;
+    public TableColumn authorsColAddress;
+    public TableColumn authorsColPhone;
+    public TableView categoriesTable;
+    public TableColumn categoriesColId;
+    public TableColumn categoriesColName;
+    public Label infoLabel;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    public void addAction(ActionEvent actionEvent) {
+    }
+
+    public void updateAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteAction(ActionEvent actionEvent) {
+    }
+
+    public void viewBooksAction(ActionEvent actionEvent) {
+    }
+}

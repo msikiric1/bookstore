88ccf8b3dd9e23f9714d2d7c8a17804edd0e46ee
msikiric1 <msikiric1@etf.unsa.ba>
2023-02-03 21:20:32 +0100

    add remaining methods to UserManager class

--- src/main/java/ba/unsa/etf/rpr/business/UserManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -3,7 +3,14 @@ package ba.unsa.etf.rpr.business;
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.BookstoreException;
+import ba.unsa.etf.rpr.exceptions.UserException;
 
+import java.util.List;
+
+/**
+ * Business logic layer for users
+ * @author Muaz Sikiric
+ */
 public class UserManager {
     public User getUser(String username, String password) throws BookstoreException {
         return DaoFactory.userDao().getUser(username, password);
@@ -11,5 +18,30 @@ public class UserManager {
 
     public User add(User user) throws BookstoreException {
         return DaoFactory.userDao().add(user);
+    }
+
+    public User getById(int id) throws BookstoreException {
+        return DaoFactory.userDao().getById(id);
+    }
+
+    public List<User> getAll() throws BookstoreException {
+        return DaoFactory.userDao().getAll();
+    }
+
+    public User update(User user) throws BookstoreException {
+        return DaoFactory.userDao().update(user);
+    }
+
+    public void delete(int id) throws BookstoreException {
+        DaoFactory.userDao().delete(id);
+    }
+
+    public void validateRegistration(String username, String password, String confirmPassword) throws UserException {
+        if(username == null || username.length() < 6)
+            throw new UserException("Username needs to be at least 6 characters.");
+        if(password == null || password.length() < 8)
+            throw new UserException("Password needs to be at least 8 characters.");
+        if(!password.equals(confirmPassword))
+            throw new UserException("Passwords do not match.");
     }
 }

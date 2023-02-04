ba328e1904feb561ba64c547b66b59db3b3a8af2
msikiric1 <msikiric1@etf.unsa.ba>
2023-02-04 12:35:40 +0100

    update book manager and add validation

--- src/main/java/ba/unsa/etf/rpr/business/BookManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/BookManager.java
@@ -4,11 +4,20 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Book;
 import ba.unsa.etf.rpr.exceptions.BookstoreException;
 
+import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * Business logic layer for books
+ * @author Muaz Sikiric
+ */
 public class BookManager {
     public List<Book> getAll() throws BookstoreException {
         return DaoFactory.bookDao().getAll();
+    }
+
+    public Book getById(int id) throws BookstoreException {
+        return DaoFactory.bookDao().getById(id);
     }
 
     public Book add(Book book) throws BookstoreException {
@@ -21,5 +30,18 @@ public class BookManager {
 
     public void delete(int id) throws BookstoreException {
         DaoFactory.bookDao().delete(id);
+    }
+
+    /**
+     * Validates title and publish date
+     * @param title book title (min. 10 characters)
+     * @param published publish date (needs to be before the current date)
+     * @throws BookstoreException
+     */
+    private void validate(String title, LocalDate published) throws BookstoreException {
+        if(title.length() < 10)
+            throw new BookstoreException("Title needs to be at least 10 characters.");
+        if(LocalDate.now().isBefore(published))
+            throw new BookstoreException("Publish date can not be in the future.");
     }
 }

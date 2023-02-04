9d535ee04234dd918331cd1cf4a27504f8165789
msikiric1 <msikiric1@etf.unsa.ba>
2023-02-04 01:03:42 +0100

    update author manager and add validation

--- src/main/java/ba/unsa/etf/rpr/business/AuthorManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/AuthorManager.java
@@ -6,12 +6,39 @@ import ba.unsa.etf.rpr.exceptions.BookstoreException;
 
 import java.util.List;
 
+/**
+ * Business logic layer for authors
+ * @author Muaz Sikiric
+ */
 public class AuthorManager {
+    public Author add(Author author) throws BookstoreException {
+        return DaoFactory.authorDao().add(author);
+    }
+
     public List<Author> getAll() throws BookstoreException {
         return DaoFactory.authorDao().getAll();
     }
 
+    public Author getById(int id) throws BookstoreException {
+        return DaoFactory.authorDao().getById(id);
+    }
+
+    public Author update(Author author) throws BookstoreException {
+        return DaoFactory.authorDao().update(author);
+    }
+
     public void delete(int id) throws BookstoreException {
         DaoFactory.authorDao().delete(id);
+    }
+
+    public void validate(String name, String address, String phone) throws BookstoreException {
+        if(name == null || name.length() < 3)
+            throw new BookstoreException("Author name needs to be at least 3 characters.");
+        if(address == null || address.length() < 12)
+            throw new BookstoreException("Author address needs to be at least 12 characters.");
+        if(phone == null || !phone.matches("[0-9]+"))
+            throw new BookstoreException("Author phone number should only contain numbers.");
+        if(phone.length() < 10)
+            throw new BookstoreException("Author phone number needs to have at least 10 numbers.");
     }
 }

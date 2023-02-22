f0df1bcacfbfa1cd57ea2e4142915e28d745d5a0
msikiric1 <msikiric1@etf.unsa.ba>
2023-02-22 16:30:22 +0100

    start writing BookManagerTest class

--- src/test/java/ba/unsa/etf/rpr/business/BookManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/business/BookManagerTest.java
@@ -1,7 +1,9 @@
 package ba.unsa.etf.rpr.business;
 
 import ba.unsa.etf.rpr.dao.BookDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Author;
 import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.domain.Category;
 import ba.unsa.etf.rpr.exceptions.BookstoreException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -17,18 +21,24 @@ import java.util.List;
  */
 public class BookManagerTest {
     private BookManager bookManager;
-    private BookDaoSQLImpl bookDaoSQLMock;
-    private List<Book> books;
+    private BookDaoSQLImpl bookDaoSQLMock = Mockito.mock(BookDaoSQLImpl.class);
+    private List<Book> books = new ArrayList<>();
 
     @BeforeEach
     public void setup() {
         bookManager = Mockito.mock(BookManager.class);
-        bookDaoSQLMock = Mockito.mock(BookDaoSQLImpl.class);
+        Author author = createAuthor(20, "Test author", "Unknown place", "123456789");
+        Category category = createCategory(10, "Test category");
+        Book book = createBook(100, "Test book 1", author, LocalDate.now(), 9.99, category);
 
+        books.addAll(Arrays.asList(
+                book,
+                createBook(101, "Test book 2", author, LocalDate.now().minusMonths(20), 5.99, category)
+        ));
     }
 
     @Test
-    public void addTest() {
+    public void addBookTest() {
 
     }
 
@@ -53,5 +63,32 @@ public class BookManagerTest {
         book.setTitle("ok");
         e = Assertions.assertThrows(BookstoreException.class, () -> bookManager.validate(book));
         Assertions.assertEquals(e.getMessage(), "Title needs to have at least 4 characters.");
+    }
+
+    private Book createBook(int id, String title, Author author, LocalDate published, Double price, Category category) {
+        Book book = new Book();
+        book.setId(id);
+        book.setTitle(title);
+        book.setAuthor(author);
+        book.setPublished(published);
+        book.setPrice(price);
+        book.setCategory(category);
+        return book;
+    }
+
+    private Author createAuthor(int id, String name, String address, String phone) {
+        Author author = new Author();
+        author.setId(id);
+        author.setName(name);
+        author.setAddress(address);
+        author.setPhone(phone);
+        return author;
+    }
+
+    private Category createCategory(int id, String name) {
+        Category category = new Category();
+        category.setId(id);
+        category.setName(name);
+        return category;
     }
 }
